322cc267f48928ea7c8a353cec186951befa7737
martinhasenbalg <martinhasen2@yahoo.com.ar>
2021-11-01 18:16:36 -0300

    readme

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          MI PRIMERA APP
+          MI PRIMER APP
         </p>
         <a
           className="App-link"
